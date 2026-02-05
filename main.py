93349592fc560b409668309a51e5101c1f23ba47
Elieser Araujo <elieseraraujo12@gmail.com>
2026-02-05 18:12:05 +0100

    Ajuste no main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from config import HOST,USER
+
 def boas_vindas():
     verde = "\033[92m"
     reset = "\033[0m"
     print(f"{verde}[SENTINELA V1.0]{reset} Sistema de Vigilância Ativo.")
+    print(f"Alvo configurado: {HOST}")
     print("-" * 30)
 
 if __name__ == "__main__":
